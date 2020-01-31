a52313122e68d333bbc1d965ef8cb8595b5f8d39
Olha Bielaia <olgabelaya1997@gmail.com>
2020-01-31 18:28:07 +0200

    add AddressDao

--- src/main/java/com/taxi/dao/OrderDao.java
+++ src/main/java/com/taxi/dao/OrderDao.java
@@ -4,6 +4,5 @@ import com.taxi.entity.Address;
 import com.taxi.entity.Order;
 
 public interface OrderDao extends CrudDao<Order> {
-    double countAmount(Order order);
-    double countDistance(AddressDao from, Address to);
+
 }
